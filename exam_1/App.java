e917a2b07f2f0294c0bc91666b9981dece4899f5
Marc Luiz Castro <111546344+CastroMarc@users.noreply.github.com>
2022-09-06 09:13:58 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ 
+       Student Me = new Student();
+       Me.Surname = "Castro";
+       Me.firstName = "Marc Luiz";
+       Me.middleInitial = 'A';
+       Me.dateOfBirth = 03/05/2004;
+       Me.studentNumber = 2022104482;
+       Me.studentemailAddress = "castroma2@students.national-u.edu.ph";
+       Me.iAmAwesome = true;
+       Me.sayMystudentNumber();
+       Me.sayMyEmailAddress();
+       Me.AmIAwesome();
+    }
+    
+ }
